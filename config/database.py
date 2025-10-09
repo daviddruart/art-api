ca83170237706bb5c4de58ee41a901e74b1b73e0
daviddruart <159968250+daviddruart@users.noreply.github.com>
2025-10-09 20:39:59 +0000

    correcciones

--- config/database.py
+++ config/database.py
@@ -1,13 +1,26 @@
+# config/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Base de datos SQLite (se crea archivo arts.db en la raíz del proyecto)
-DATABASE_URL = "sqlite:///./arts.db"
+# Ruta del archivo SQLite (puedes cambiar el nombre si quieres)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./artcook.db"
 
+# Conexión con SQLite
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# Sesión de trabajo con la base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base para los modelos
 Base = declarative_base()
+
+# Dependencia para obtener la sesión en cada request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
